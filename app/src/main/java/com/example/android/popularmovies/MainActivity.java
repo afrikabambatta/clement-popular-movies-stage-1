11dce2411f2e6983059779ef93f8fba235f20f45
Clement <ctintran@gmail.com>
2020-03-30 10:51:44 -0700

    Create stub code to give the recycler view a click listener
    A toast is created when clicking on the viewholders

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -5,14 +5,17 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import java.util.LinkedList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements MovieGridAdapter.MovieItemClickListener{
 
     private final LinkedList<String> mDummyMovieData = new LinkedList<>();
     private RecyclerView mRecyclerView;
     private MovieGridAdapter mAdapter;
+    private Toast mToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,20 +26,25 @@ public class MainActivity extends AppCompatActivity {
             mDummyMovieData.add("Movie #" + (i+1));
         }
 
-        // NOTE: get a handle to the recycler view
+        // get a handle to the recycler view
         mRecyclerView = findViewById(R.id.rv_movies);
-        // NOTE: give the recycler default layout manager
+        // give the recycler default layout manager
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        // NOTE: let the recycler all viewholders are the same size for optimization
+        // let the recycler all viewholders are the same size for optimization
         mRecyclerView.setHasFixedSize(true);
-        // NOTE: create adapter for recycler view and give it dummy data
-        mAdapter = new MovieGridAdapter(mDummyMovieData);
-        // NOTE: set the adapter on the recycler
+        // create adapter for recycler view and give it dummy data
+        mAdapter = new MovieGridAdapter(mDummyMovieData, this);
+        // set the adapter on the recycler
         mRecyclerView.setAdapter(mAdapter);
-
-
-
     }
 
+    @Override
+    public void onMovieItemClick(int clickedItemIndex) {
+        String toastMessage = "Item #" + (clickedItemIndex + 1) + " clicked.";
+        if(mToast != null)
+            mToast.cancel();
+        mToast = Toast.makeText(this, toastMessage, Toast.LENGTH_LONG);
 
+        mToast.show();
+    }
 }
--- app/src/main/java/com/example/android/popularmovies/MovieGridAdapter.java
+++ app/src/main/java/com/example/android/popularmovies/MovieGridAdapter.java
@@ -15,48 +15,108 @@ public class MovieGridAdapter extends RecyclerView.Adapter<MovieGridAdapter.Movi
 
     private final LinkedList<String> mMovieList;
 
-    public MovieGridAdapter(LinkedList<String> movieList){
-        // NOTE: I can pass in the movieList or just its size
-        this.mMovieList = movieList;
+    final private MovieItemClickListener mOnClickListener;
+
+    /**
+     * Interface for ClickListener
+     */
+    public interface MovieItemClickListener {
+        void onMovieItemClick(int clickedItemIndex);
     }
+    /**
+    * Constructor for Adapter
+    *
+    */
+    public MovieGridAdapter(LinkedList<String> movieList, MovieItemClickListener listener){
+        // I can pass in the movieList or just its size
+        mMovieList = movieList;
+        mOnClickListener = listener;
+    }
+
+    /**
+     * This method basically inflates the viewholder
+     *
+     * @param viewGroup
+     * @param viewType
+     * @return
+     */
 
     @NonNull
     @Override
     public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
-        // NOTE: Get the context of the application
+        // Get the context of the application
         Context context = viewGroup.getContext();
-        // NOTE: Create an inflater in this context
+        // Create an inflater in this context
         LayoutInflater inflater = LayoutInflater.from(context);
-        // NOTE?: Use the inflater to inflate the movieposter_item in viewGroup
+        // Use the inflater to inflate the movieposter_item in viewGroup
         View view = inflater.inflate(R.layout.movieposter_item, viewGroup, false);
 
-        // NOTE: Create a movie view holder passing in this inflated view
+        // Create a movie view holder passing in this inflated view
         return new MovieViewHolder(view);
     }
 
-    // NOTE: When the adapter binds data to the viewholder it calls this passing it position in rv
+    /**
+     * Recycler view calls when new data needs to be binded to the viewholder
+     *
+     * @param holder
+     * @param position
+     */
+
+    // When the adapter binds data to the viewholder it calls this passing it position in rv
     @Override
     public void onBindViewHolder(@NonNull MovieViewHolder holder, int position) {
-        // NOTE: pass in the position to retrieve movie name at position
+        // pass in the position to retrieve movie name at position
         String mCurrent = mMovieList.get(position);
-        // NOTE: recycler takes the holder and gives it new text
+        // recycler takes the holder and gives it new text
         holder.movieItemView.setText(mCurrent);
     }
 
-    // NOTE: Recycler view needs to know how many items in the data to make space for it
+    /**
+     * Returns the total items in data. Needed by recycler view to know how many items to display.
+     *
+     * @return
+     */
+
+    // Recycler view needs to know how many items in the data to make space for it
     @Override
     public int getItemCount() {
         return mMovieList.size();
     }
 
-    // NOTE: Nested viewholder class because only used within this adapter
-    class MovieViewHolder extends RecyclerView.ViewHolder{
+    /**
+     * Class definition for view holder. Also, implements a ClickListener for interaction
+     *
+     */
+
+    // Nested viewholder class because only used within this adapter
+    class MovieViewHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener{
+
+
         private final TextView movieItemView;
 
+        /**
+         * Constructor that assigns movieItem xml to viewholder
+         *
+         * @param itemView
+         */
         public MovieViewHolder(View itemView){
             super(itemView);
-            // NOTE: This viewholder is made up of a text view, for now
+            // This viewholder is made up of a text view, for now
             movieItemView  = itemView.findViewById(R.id.movie_poster);
+            itemView.setOnClickListener(this);
+        }
+
+        /**
+         * Clicklistener function that helps Recycler know which viewholder was clicked
+         *
+         * @param view
+         */
+        @Override
+        public void onClick(View view) {
+            int clickedPosition = getAdapterPosition();
+            mOnClickListener.onMovieItemClick(clickedPosition);
+
         }
     }
 }
